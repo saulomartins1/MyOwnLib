d9cca58ab08f2aaeb276644a4332f70bc18bafcd
d3Niro <saulomartins2020@gmail.com>
2023-12-31 17:45:35 -0300

    Initial commit from Create Next App

---
+++ app/components/SideNav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SideNav() {
+    return (
+        <aside>SideNav</aside>
+    )
+}
+
+export default SideNav
---
+++ app/db/books.ts
@@ -0,0 +1,26 @@
+export const dataBooks = [
+    {
+        id: 1,
+        title: "Book 1",
+        pdfPath: "/pdfs/book1.pdf",
+        pageCount: 100,
+    },
+    {
+        id: 2,
+        title: "Book 2",
+        pdfPath: "/pdfs/book1.pdf",
+        pageCount: 100,
+    },
+    {
+        id: 3,
+        title: "Book 3",
+        pdfPath: "/pdfs/book1.pdf",
+        pageCount: 100,
+    },
+    {
+        id: 4,
+        title: "Book 4",
+        pdfPath: "/pdfs/book1.pdf",
+        pageCount: 100,
+    },
+]
