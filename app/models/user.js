241853abdb371944425d874c0e1c8d18d552b5be
d3Niro <saulomartins2020@gmail.com>
2024-01-01 17:37:08 -0300

    chore: User model

---
+++ app/models/user.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: false,
+        },
+        books: [Number],
+    }, { timestamp: true }
+);
+
+export default mongoose.models.User || mongoose.modal('User', userSchema);
