143861d2343aae4f63de17d56bc0351af184e4b6
d3Niro <saulomartins2020@gmail.com>
2024-01-05 14:00:02 -0300

    style: header SearchBar and filter options

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import NavLink from 'next/link'
 import SideNav from './components/SideNav'
+import { InputSearchBook, SearchBook_Filter, SearchBook_Grid } from './components/ui/Input'
 
 async function Home() {
 
@@ -9,9 +9,12 @@ async function Home() {
          <div className='flex gap-4'>
             <SideNav />
             <div className='flex-grow'>
-               <header className='bg-[blue] flex items-center justify-center gap-8'> <nav>
-                  <NavLink className='mr-8' href="/">Search for a book</NavLink>
-               </nav>
+               <header className='p-4 bg-dark2 flex items-center justify-center gap-8'>
+                  <nav className='flex'>
+                     <InputSearchBook />
+                     <SearchBook_Filter />
+                     <SearchBook_Grid />
+                  </nav>
                </header>
                <main className='bg-[red]'>Books here</main>
             </div>
